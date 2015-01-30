13e4e8125dece29f2c2f4199e3a0a8110a334d97
Drago Rosson <drago@rosson.me>
2015-01-30 11:04:37 -0600

    Add removal of vim-tiny

--- linux.sh
+++ linux.sh
@@ -2,8 +2,11 @@
 
 ECHO_BLUE () { echo -e "\033[0;34m${1}\033[0m"; }
 
-ECHO_BLUE "Installing tmux and zsh"
-sudo apt-get install -y tmux zsh
+ECHO_BLUE "Uninstalling vim-tiny"
+sudo apt-get remove -y vim-tiny
+
+ECHO_BLUE "Installing vim, tmux, and zsh"
+sudo apt-get install -y vim tmux zsh
 
 ECHO_BLUE "Installing oh-my-zsh"
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
