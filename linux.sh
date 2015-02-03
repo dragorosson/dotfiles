99a181374d129038ae2a90b1811c2636e928904e
Drago Rosson <drago@rosson.me>
2015-02-03 15:03:12 -0600

    Change default shell to zsh

--- linux.sh
+++ linux.sh
@@ -7,6 +7,7 @@ sudo apt-get remove -y vim-tiny
 
 ECHO_BLUE "Installing vim, tmux, and zsh"
 sudo apt-get install -y vim tmux zsh
+sudo chsh -s /bin/zsh $USER
 
 ECHO_BLUE "Installing oh-my-zsh"
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
